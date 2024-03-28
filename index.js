f9df2b79265c15319056efa979acd20340513995
Marcel Schoombee <marcelsch01@yahoo.com>
2024-03-28 15:21:40 +0200

    get order list

--- index.js
+++ index.js
@@ -20,30 +20,19 @@ function displayMenuItems(menu) {
         // Create an element to represent the category
         const listItemElement = document.createElement('li');
         // Set the text content of the category element to the category name
-
-        // Append the category element to the menu container
-
-        // Create an element to represent a list of items
-
-        // Append a list of items element to the menu container
-
-        // Loop through the items in the category and create list items
-
-            // Create a list item element
-
-            // Set the text content of the list item element to the item name
-
-            // Attach a click event listener to the list item to add it to the order
-
-            // Append the list item to the list of items
-
-            
+        listItemElement.textContent = itemName;
+        listItemElement.addEventListener('click', () => addToOrder(itemName));
+        itemListElement.appendChild(listItemElement);
+        menuContainer.appendChild(itemListElement);
+    });
+        }
+    }   
 }
-
 // Callback function for adding an item to the order
 function addToOrder(itemName) {
     // Get the order items list and the order total element from the HTML
-
+    const orderItemsList = document.getElementById("order-items");
+    const orderTotalElement = document.getElementById("order-total");
     // Create a list item for the order
 
     // Set the text content of the list item to the item name
