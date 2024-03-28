20f46c70c20504790fe6eafc1f9c7a08c157fb64
Marcel Schoombee <marcelsch01@yahoo.com>
2024-03-28 15:23:23 +0200

    Set text content of the list

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function addToOrder(itemName) {
     // Create a list item for the order
     const orderItem = document.createElement('li');
     // Set the text content of the list item to the item name
-
+    orderItem.textContent = itemName;
     // Append the list item to the order items list
 
     // Calculate and update the total price
